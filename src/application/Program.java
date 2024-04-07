a4071a9c30426325d72ff8386ba02f46e44a267d
Vitor Lopes <vitornaruto1990@gmail.com>
2024-04-07 10:44:47 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,10 @@
 package application;
 
+
 import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -19,12 +19,14 @@ public class Program {
 		
 		System.out.println(seller);
 		
-		System.out.println("=== TEST 2: seller findById ===");
-		Department department = new Department(2, null);
-		List<Seller> list = sellerDao.findByDepartment(department);
+		System.out.println("=== TEST 3: seller findAll ===");
+		List<Seller>list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		
+		
 	}
 	
 }
